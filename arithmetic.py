aea40eec52d7129c8a1ae64a961229edd52fc122
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:18:20 -0700

    Most of them are working

--- arithmetic.py
+++ arithmetic.py
@@ -1,23 +1,31 @@
 def add(num1, num2):
-    pass
+    
+    return int(num1 + num2)
 
 def subtract(num1, num2):
-    pass
+    
+    return int(num1) - int(num2)
 
 def multiply(num1, num2):
-    pass
+    
+    return int(num1) * int(num2)
 
 def divide(num1, num2):
-    pass
+    
+    return int(num1) / int(num2)
 
 def square(num1):
-    pass
+    
+    return int(num1) * int(num1)
 
 def cube(num1):
-    pass
+
+    return int(num1) * int(num1) * int(num1) 
 
 def power(num1, num2):
-    pass
+    
+    return int(num1) ** int(num2)
 
 def mod(num1, num2):
-    pass
+    
+    return int(num1) % int(num2)
